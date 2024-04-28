7383f45998bf6b18b586eace7a7372b761d22d54
kacperrr <59144349+0273574@users.noreply.github.com>
2024-04-28 19:22:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
     var nav = document.getElementById("myNav");
-    if (nav.style.width === "80%") {
+    if (nav.style.width === "50%") {
         nav.style.width = "0";
     } else {
-        nav.style.width = "80%";
+        nav.style.width = "50%";
     }
 }
 
